f96ce2dfec881bc5671ec69a40ca716c5610068a
abdullah-hasham-code <76914757+abdullah-hasham-code@users.noreply.github.com>
2024-08-10 16:01:10 +0500

    purchase and sale work

--- src/app/sale-till-open/sale-till-open.component.ts
+++ src/app/sale-till-open/sale-till-open.component.ts
@@ -6,52 +6,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./sale-till-open.component.css']
 })
 export class SaleTillOpenComponent {
-  tillOpenForm:any={};
-
+  tillOpenForm:any={    five000: 5000,five000v: 0,    five000t: 0,    one0000: 1000,    one0000v: 0,    one0000t: 0,
+    five00: 500,    five00v: 0,    five00t: 0,    one00: 100,
+    one00v: 0,    one00t: 0,    five0: 50,
+    five0v: 0,    five0t: 0,    two0: 20,    two0v: 0,    two0t: 0,
+    one0: 10,    one0v: 0,    one0t: 0,    five: 5,    fivev: 0,
+    fivet: 0,    two: 2,    twov: 0,    twot: 0,    one: 1,    onev: 0,
+    onet: 0};
+    formData:any={};
+    updateTotal() {
+      this.formData.amount = this.tillOpenForm.five000t + this.tillOpenForm.one0000t + 
+                             this.tillOpenForm.five00t + this.tillOpenForm.one00t + 
+                             this.tillOpenForm.five0t + this.tillOpenForm.two0t + 
+                             this.tillOpenForm.one0t + this.tillOpenForm.fivet + 
+                             this.tillOpenForm.twot + this.tillOpenForm.onet;
+    }
   amountfive000Changes(e:any){ 
     this.tillOpenForm.five000t=0; 
     this.tillOpenForm.five000t=e.target.value*5000;
+    this.updateTotal();
   }
   amountone000Changes(e:any){ 
     this.tillOpenForm.one0000t=0; 
     this.tillOpenForm.one0000t=e.target.value*1000;
-
+    this.updateTotal();
   }
   amountfive00Changes(e:any){
     this.tillOpenForm.five00t=0; 
     this.tillOpenForm.five00t=e.target.value*500;
+    this.updateTotal();
   }
   amountone00Changes(e:any){
     this.tillOpenForm.one00t=0; 
     this.tillOpenForm.one00t=e.target.value*100;
+    this.updateTotal();
   }
   amount50Changes(e:any){
     this.tillOpenForm.five0t=0; 
     this.tillOpenForm.five0t=e.target.value*50;
+    this.updateTotal();
   }
   amounttwo0Changes(e:any){
     this.tillOpenForm.two0t=0; 
     this.tillOpenForm.two0t=e.target.value*20;
+    this.updateTotal();
   }
-  // amountone0Changes(e:any){
-  //   this.tillOpenForm.one0t=0; 
-  //   this.tillOpenForm.one0t=e.target.value*500;
-  // }
   amountfiveChanges(e:any){
     this.tillOpenForm.fivet=0; 
     this.tillOpenForm.fivet=e.target.value*5;
+    this.updateTotal();
   }
   amounttwoChanges(e:any){
     this.tillOpenForm.twot=0; 
     this.tillOpenForm.twot=e.target.value*2;
+    this.updateTotal();
   }
   amountOne0Changes(e:any){
     this.tillOpenForm.one0t=0; 
     this.tillOpenForm.one0t=e.target.value*10;
+    this.updateTotal();
   }
   amountOneChanges(e:any){
     this.tillOpenForm.onet=0; 
     this.tillOpenForm.onet=e.target.value*1;
+    this.updateTotal();
   }
   add(){}
   cancel(){}
